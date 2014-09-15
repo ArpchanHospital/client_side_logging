64ec3b0ec8bb292ea49e696aeab783d6ed88c590
mihirk <mihir.khatwani@gmail.com>
2014-09-15 15:34:01 +0530

    Chethan, Mihir | #380 | Logging post data

--- client_side_logging.py
+++ client_side_logging.py
@@ -1,9 +1,18 @@
-from flask import Flask
+import logging
+
+from flask import Flask, request
+
+CLIENT_SIDE_LOG_FILE = '/var/log/client-side.log'
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+
+@app.route("/", methods=["POST"])
+def client_side_logging():
+    logging.basicConfig(filename=('%s' % CLIENT_SIDE_LOG_FILE), level=logging.ERROR)
+    logging.error(request.data)
+    return request.data
+
 
 if __name__ == "__main__":
     app.run()
