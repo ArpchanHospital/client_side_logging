d44fa576386223652b731a85d9e8ecd8f035a61e
mihirk <mihir.khatwani@gmail.com>
2014-09-16 16:12:04 +0530

    Chethan, Mihir | #380 | Adding timed rolling logs

--- client_side_logging.py
+++ client_side_logging.py
@@ -1,17 +1,28 @@
 import logging
+from logging.handlers import TimedRotatingFileHandler
+
 
 from flask import Flask, request, make_response
 
 
-CLIENT_SIDE_LOG_FILE = '/var/log/client-side.log'
+CLIENT_SIDE_LOG_FILE = 'client-side.log'
 
 app = Flask(__name__)
+logger = logging.getLogger("Client Side ")
+logger.setLevel(logging.ERROR)
+log_handler = TimedRotatingFileHandler(CLIENT_SIDE_LOG_FILE, when='s', interval=5)
+log_handler.setLevel(logging.ERROR)
+log_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+log_handler.setFormatter(log_formatter)
+logger.addHandler(log_handler)
+
 
 
 @app.route("/", methods=["POST"])
 def client_side_logging():
-    logging.basicConfig(format='%(levelname)s: %(asctime)s  ---  %(message)s', filename=CLIENT_SIDE_LOG_FILE, level=logging.ERROR)
-    logging.error(request.data)
+    # logging.basicConfig(format='%(levelname)s: %(asctime)s  ---  %(message)s', filename=CLIENT_SIDE_LOG_FILE, level=logging.ERROR)
+    # logging.error(request.data)
+    logger.error(request.data)
     return make_response(request.data, 201)
 
 
